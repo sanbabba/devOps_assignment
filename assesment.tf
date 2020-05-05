28cd9483917ed2516a40959f5322c38e4a930486
Manjunath Shet <manjunath.shet@gmail.com>
2020-05-06 03:37:00 +0530

    Update assesment.tf
    Update the entry for ansible installation

--- assesment.tf
+++ assesment.tf
@@ -21,6 +21,7 @@ resource "aws_instance" "web" {
 		#! /bin/bash
         sudo apt-get update
 		sudo apt-get install -y git
+		sudo apt-get install ansible
 		sudo apt-get install -y apache2
 		sudo systemctl start apache2
 		sudo systemctl enable apache2
