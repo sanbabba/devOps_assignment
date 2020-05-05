97ec294fd7218fba6f97abaeec33bd2243dbcdf0
manjunath.shet@gmail.com <manjunath.shet@gmail.com>
2020-05-05 23:35:46 +0530

    assess

---
+++ assesment.tf
@@ -0,0 +1,140 @@
+#https://www.terraform.io/docs/providers/aws/r/cloudwatch_metric_alarm.html
+#
+#=========================================================================================
+#==================================================================================
+
+provider "aws" {
+  access_key = "aws_access_key_id"
+  secret_key = "aws_secret_access_key_id"
+  region     = "ap-south-1"
+}
+data "aws_availability_zones" "all" {}
+### Creating EC2 instance
+resource "aws_instance" "web" {
+  ami                    = "${lookup(var.amis, var.region)}"
+  count                  = "${var.count1}"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  source_dest_check      = false
+  instance_type          = "t2.micro"
+  tags {
+    Name = "${format("web-%03d", count.index + 1)}"
+  }
+}
+### Creating Security Group for EC2
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+## Creating Launch Configuration
+resource "aws_launch_configuration" "example" {
+  image_id        = "${lookup(var.amis, var.region)}"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+  key_name        = "${var.key_name}"
+  user_data       = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+## Creating AutoScaling Group
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  min_size             = 1
+  max_size             = 2
+  load_balancers       = ["${aws_elb.example.name}"]
+  health_check_type    = "ELB"
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+## Security Group for ELB
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+### Creating ELB
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  security_groups    = ["${aws_security_group.elb.id}"]
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:8080/"
+  }
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "8080"
+    instance_protocol = "http"
+  }
+}
+
+
+#=========================================================================================
+# Added code from
+# https://www.terraform.io/docs/providers/aws/r/cloudwatch_metric_alarm.html
+#=========================================================================================
+resource "aws_cloudwatch_metric_alarm" "foobar" {
+  alarm_name                = "terraform-test-foobar"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  threshold                 = "10"
+  alarm_description         = "Request error rate has exceeded 10%"
+  insufficient_data_actions = []
+
+  metric_query {
+    id          = "e1"
+    expression  = "m2/m1*100"
+    label       = "Error Rate"
+    return_data = "true"
+  }
+
+  metric_query {
+    id = "m1"
+
+    metric {
+      metric_name = "100"
+      namespace   = "AWS/ELB"
+      period      = "120"
+      stat        = "Sum"
+      unit        = "Count"
+
+      dimensions = {
+        LoadBalancer = "app/web"
+      }
+    }
+  }
+}
